86bf42595166a93bc6f618d6634fd2340f0536de
Muzza <murster972@yahoo.com>
2017-10-15 01:24:21 +0100

    commit

--- client.py
+++ client.py
@@ -7,7 +7,41 @@ from colours import Colours
 
 class Client:
     def __init__(self):
-        pass
+        try:
+            created = False
+
+            server_addr = (input("Server IP: "), int(input("Server Port: ")))
+
+            self.c_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.c_sock.connect(server_addr)
+
+            created = True
+
+            print("{}[+]{} Client connected to server".format(Colours.green, Colours.white))
+
+            kill_code = self.c_sock.recv(1024)
+
+            #send server client ip, hostname and port
+
+
+            #listen for cmds from server
+            while True:
+                cmd = self.c_sock.recv(1024).decode()
+
+
+        except ValueError:
+            print("{}[-] {}Invalid server address".format(Colours.red, Colours.white))
+            sys.exit(-1)
+        except ConnectionRefusedError:
+            print("{}[-] {}Unable to connect to server".format(Colours.red, Colours.white))
+        except socket.error as err:
+            print("{}[-]{} The folling error occured: {}".format(Colours.red, Colours.white, err))
+        except KeyboardInterrupt:
+            pass
+        finally:
+            if created:
+                self.c_sock.close()
+                print("{}[*]{} Client closed".format(Colours.red, Colours.white))
 
 if __name__ == '__main__':
-    Server()
+    Client()
--- server.py
+++ server.py
@@ -14,37 +14,63 @@ class Server:
     clients = {}
 
     #a dict of all commands to be sent to clients
+    #{"client_id": (cmd_id, cmd)}
     command_queue = {}
+    command_results = {}
+
+    sock = ""
+    addr = ""
+
+    #code sent to clients to end connection
+    kill_code = getrandbits(32)
+
+    BUFF_SIZE = 1024
 
     def __init__(self):
         try:
             sock_created = False
-            server_addr = (input("IP Address: "), int(input("Port Number: ")))
+            Server.addr = (input("IP Address: "), int(input("Port Number: ")))
 
             #create server socket and binds to address
-            self.server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            self.server_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-            self.server_sock.bind(server_addr)
+            Server.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            Server.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            Server.sock.bind(Server.addr)
+            Server.sock.listen(5)
 
             sock_created = True
 
             #start client listener
-            listen =
+            #listen = Thread(target=ClientHandler, daemon=True)
+            #listen.start()
+
+            menu = {"1": "", "2": "", "3": "", "4": lambda: sys.exit(0)}
+
+            while True:
+                try:
+                    os.system("clear")
+                    print(Colours.white + "[1] - Show Clients\n[2] - Send command\n[3] - View Results\n[4] - exit")
+                    opt = input("Option: ")
+
+                    menu[opt]()
+
+                except KeyError:
+                    print("\n{}[x] {}Invalid option: {}{}".format(Colours.red, Colours.white, Colours.blue, opt))
+                    pause = input("\n" + Colours.blue + "Enter to continue...")
 
 
         except ValueError:
-            print("{}[-] Server Error: Invalid address{}".format(Colours.red, Colours.white))
-            sys.exit(-1)
+            print("{}[x] Server Error: Invalid address{}".format(Colours.red, Colours.white))
         except PermissionError:
-            print("{}[-] Server Error: Don't have permisson to bind server to address{}".format(Colours.red, Colours.white))
-            sys.exit(-1)
+            print("{}[x] Server Error: Don't have permisson to bind server to address{}".format(Colours.red, Colours.white))
+        except socket.error as err:
+            print("{}[x]{} The folling error occured: {}".format(Colours.red, Colours.white, err))
         except KeyboardInterrupt:
             pass
-
-        #closed server socket if socket has been created
-        if sock_created: self.server_sock.close()
-
-        print("\n{}[*] {}Server closed".format(Colours.blue, Colours.white))
+        finally:
+            #closed server socket if socket has been created
+            if sock_created:
+                Server.sock.close()
+                print("\n{}[*] {}Server closed".format(Colours.blue, Colours.white))
 
     ''' Listens for clients connecting to the server and passes them off to the client handler method '''
     def client_listner(self):
@@ -59,27 +85,42 @@ class Server:
             Server.command_queue[ID] = []
             new_client = Thread(target=Client, args=[ID], daemon=True)
 
-''' Instance of a client, sends messages and recives replys from clients etc. '''
-class Client(Server):
-    def __init__(self, ID):
-        self.id = ID
-        self.messages = []
-
-        #start thread to recive messages from client
-        r = Thread(target=self.recive, daemon=True)
-        r.start()
-
-        #check for messages from client and for any commands to send client
+''' Listens for new clients, recieves and sends messages from clients '''
+class ClientHandler(Server):
+    def __init__(self):
+        #listens for clients
         while True:
-            if self.messages:
-                pass
-            if Server.command_queue[ID]:
-                pass
-            time.sleep(10)
+            print("{}[+]{} Server listening at for clients at: {}".format(Colours.green, Colours.white, Server.addr))
+            c_sock, c_addr = Server.server_sock.listen()
+            c_sock.send(kill_code.encode())
 
-    def recive(self):
+            try:
+                #recv client info - hostname, ip, addr as tuple
+                c_info = eval(c_sock.recv(Server.BUFF_SIZE).decode())
+
+            except Exception:
+                print("{}[-]{} Error occured while recieving client information: {}".format(Colours.red, Colours.white, c_info))
+                c_sock.send(kill_code.encode())
+
+            self.c_id = randbits(32)
+
+            #for the very small chance of a repeat id
+            while self.c_id in Server.clients: self.c_id = randbits(32)
+
+            #{"client": (hostname, ip_addr, port)}
+            Server.clients[self.c_id] = ()
+
+            #recieve messages
+
+            #send messages
+
+    def recieve(self):
+        pass
+
+    def send(self):
         while True:
-            pass
+            if command_queue[self.client_id]:
+                pass
 
 
 if __name__ == '__main__':
